d6c65bf325c25e8e203ad28f616a2f27a96d9df1
Nico <Nico.Westerbeck@mailbox.tu-dresden.de>
2015-10-27 13:20:46 +0100

    Okay, I give up -.-

--- alastair_cookie/settings.py
+++ alastair_cookie/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'alastair_cookie',
         'USER' : 'django',
-        'PASSWORD' : 'YNWQQfJynKZ3sjhQ',
+        'PASSWORD' : '',
     }
 }
 
